8a9c15766595b45ea719f61d81657539e3fe7976
ostrrovska <ostrovskakatia@gmail.com>
2025-07-28 22:05:14 +0300

    Test editing functionality

--- controllers/employee_controller.py
+++ controllers/employee_controller.py
@@ -14,3 +14,17 @@ def list_employees():
 def add():
     employee_service.add(request.form['name'], request.form['age'])
     return redirect(url_for('employee.list_employees'))
+
+@employee_controller.route('/edit/<string:name>', methods = ['GET'])
+def edit_employee(name):
+    employee = employee_service.get_by_name(name)
+    if not employee:
+        return redirect(url_for('employee.list_employees'))
+    return render_template('employee_edit.html', employee = employee)
+
+@employee_controller.route('/update/<string:name>', methods = ['POST'])
+def update(name):
+    age = request.form['age']
+    if age:
+        employee_service.update(name, age)
+    return redirect(url_for('employee.list_employees'))
--- services/employee_service.py
+++ services/employee_service.py
@@ -9,5 +9,18 @@ class EmployeeService:
     def get_all(self):
         return self._employees
 
+    def get_by_name(self, name):
+        for employee in self._employees:
+            if employee.name == name:
+                return employee
+        return None
+
     def add(self, name, age):
         self._employees.append(Employee(name, age))
+
+    def update(self, name, age):
+        employee = self.get_by_name(name)
+        if employee:
+            employee.age = age
+            return True
+        return False
